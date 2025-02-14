3e1d1083ade4478ceba188cdfccd5a650815f464
Vishal Kumar <125488544+vk041499@users.noreply.github.com>
2025-02-14 10:43:32 +0530

    updated code

--- 07_projects/project2Solution.js
+++ 07_projects/project2Solution.js
@@ -6,16 +6,20 @@ form.addEventListener('submit', function (e) {
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
 
-
   if (height === '' || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
-  }
-  else if (weight === '' || weight < 0 || isNaN(weight)) {
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
     results.innerHTML = `Please give a valid weight ${weight}`;
-  } 
-  else {
+  } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     // show the result
     results.innerHTML = `<span>${bmi}</span>`;
+    if (bmi < 18.6) {
+      results.innerHTML = `<span>${bmi} Under Weight </span>`;
+    } else if (bmi > 18.6 && bmi < 24.9) {
+      results.innerHTML = `<span>${bmi} Normal Range </span>`;
+    } else {
+      results.innerHTML = `<span>${bmi} Over Weight </span>`;
+    }
   }
 });
